c4a5bdafd2773e033b8d4293ca49855f10e66ffa
MuhammadBiLaLJaMiL <muhamadbilaljamil@gmail.com>
2022-07-16 02:16:22 +0500

    rafa

---
+++ src/pages/main/Main.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { ReactComponent as Line } from '../../icons/Line.svg';
+
+import Animation from '../../components/animation/Animation';
+import Browse from '../../components/browse/Browse';
+import End from '../../components/end/End';
+import Intro from '../../components/Intro/Intro';
+import Preview from '../../components/preview/Preview';
+import Share from '../../components/share/Share';
+// import Timer from '../../components/timer/Timer'
+
+import './main.css';
+import Footer from '../../components/footer/Footer';
+import CarouselBanner from '../../components/Intro/CarouselBanner';
+
+const Main = () => {
+  return (
+    <div className="main">
+      <CarouselBanner />
+
+      <div className="componentFirst">
+        <div className="componentSecondMain">
+          <div className="elapse" />
+          <Line className="Line1" />
+          <Line className="Line2" />
+          <Line className="Line3" />
+          {/* <Timer /> */}
+          <Preview />
+          <Share />
+          <Browse />
+          <Intro />
+        </div>
+      </div>
+      <End />
+      <Footer />
+      <Animation />
+    </div>
+  );
+};
+
+export default Main;
